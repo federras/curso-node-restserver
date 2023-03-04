ea8588000e8c5dbe9a7aa78f7de98810a379650f
Fede Rascon <federascon@gmail.com>
2023-03-04 17:06:42 -0300

    Arreglo en conection mongoDB 2

--- database/config.js
+++ database/config.js
@@ -5,19 +5,12 @@ const dbConection = async () => {
     try {
         mongoose.set('strictQuery', false);
 
-        // await mongoose.createConnection(process.env.MONGODB_CNN, {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true
-        // }).asPromise();
-
-        // await mongoose.connect( process.env.MONGODB_CNN, {
-        //     // useNewUrlParser: true,
-        //     // useUnifiedTopology: true
-        //     // useCreateIndex: true,
-        //     // useFindAndModify: false
-        // });
-
-        await mongoose.connect( process.env.MONGODB_CNN);
+        await mongoose.connect( process.env.MONGODB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+            // useCreateIndex: true,
+            // useFindAndModify: false
+        });
         
         console.log('Base de datos online')
 
